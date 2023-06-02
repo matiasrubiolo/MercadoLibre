1beca73089b8661b987aff092860199cfffb8b98
Rubiolo, Matias <maty_rubiolo@outlook.com>
2023-06-02 13:01:15 -0300

    add api de productos

--- main.js
+++ main.js
@@ -1,18 +1,109 @@
 document.addEventListener("DOMContentLoaded", function() {
     let pregunta = document.getElementById("enviar");
-    if(pregunta){
-        pregunta.addEventListener("click",comentarios);
+    if (pregunta) {
+      pregunta.addEventListener("click", comentarios);
     }
-
+  
     let favorito = document.getElementById("favorito");
-    if(favorito){
-        favorito.addEventListener("click",misfavorito);
+    if (favorito) {
+      favorito.addEventListener("click", misfavorito);
     }
-
+  
     let buttonbuscador = document.getElementById("buttonBuscador");
-    buttonbuscador.addEventListener("click",buscador);
-    cargarcantidad();
-});
+    buttonbuscador.addEventListener("click", buscador);
+  
+    if (window.location.href.includes('buscador.html')) {
+      cargarcantidad();
+    }
+  
+    if (window.location.href.includes('index.html')) {
+      cargaproductos();
+    }
+  
+    let idElement = document.getElementById("id");
+    if (idElement) {
+      cargapagina();
+    }
+  
+    async function cargapagina() {
+      let id = parseInt(document.getElementById("id").textContent);
+  
+      let selectElement = document.getElementById("ram");
+      selectElement.addEventListener("change", updatePage); // Escucha el evento change en el select
+  
+      async function updatePage() {
+        let selectedIndex = selectElement.selectedIndex;
+        let selectedOption = selectElement.options[selectedIndex].value;
+        let json = await cargarurl("https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products");
+        json.forEach(producto => {
+          producto.notebooksTypes.forEach(notebook => {
+            if (producto.id === id) {
+              let img = document.getElementById("img");
+              let title = document.getElementById("title");
+              let price = document.getElementById("price");
+              let description = document.getElementById("description");
+              let fabricante = document.getElementById("linkfabricante");
+              img.src = producto.image_url;
+              if (selectedOption == "8GB") {
+                title.textContent = producto.title + " " + producto.notebooksTypes[0].ramAmount;
+                price.textContent = "$" + producto.notebooksTypes[0].price;
+              } else {
+                title.textContent = producto.title + " " + producto.notebooksTypes[1].ramAmount;
+                price.textContent = "$" + producto.notebooksTypes[1].price;
+              }
+              description.textContent = producto.description;
+              fabricante.textContent = producto.factory_url;
+              fabricante.href = producto.factory_url;
+              document.title = producto.title;
+            }
+          });
+        });
+      }
+  
+      updatePage(); // Llama a la función inicialmente para establecer los valores correctamente
+    }
+  });
+  
+
+async function cargaproductos() {
+    let grilla = document.getElementById("grilla");
+    grilla.classList.add("grilla");
+    let json = await cargarurl("https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products");
+
+    json.forEach(producto => {
+        producto.notebooksTypes.forEach(notebook => {
+            let link = document.createElement("a");
+            let articulo = document.createElement("div");
+            articulo.classList.add("articulo");
+            articulo.id = producto.id;
+            let imagen = document.createElement("img");
+            let divider = document.createElement("div");
+            divider.classList.add("divider");
+            let precio = document.createElement("p");
+            precio.classList.add("price");
+            let titulo = document.createElement("p");
+            titulo.classList.add("title");
+
+            let textotitulo = producto.title + " " + notebook.ramAmount;
+            let textoprecio = notebook.price; 
+            let textoimagen = producto.image_url;
+            let textolink = producto.title.replace(/ /g, "_") + ".html";
+
+            imagen.src = textoimagen;
+            precio.textContent = "$" + textoprecio;
+            titulo.textContent = textotitulo;
+            link.href = textolink;
+
+            articulo.appendChild(imagen);
+            articulo.appendChild(divider);
+            articulo.appendChild(precio);
+            articulo.appendChild(titulo);
+            link.appendChild(articulo)
+            grilla.appendChild(link);
+        });
+    });
+}
+
 
 async function cargarcantidad() {
     let json = await cargarurl("https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/statistics");
